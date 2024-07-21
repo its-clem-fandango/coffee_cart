188688aa9f7dc83bf319e5eecab2457c0dd0c034
Clem Fandango <drosshere@gmail.com>
2024-07-21 20:49:32 +0200

    Add Row Widget

---
+++ lib/home.dart
@@ -0,0 +1,70 @@
+import "package:flutter/material.dart";
+
+/*NOTES: stl creates a stateless widget. Start all classes w/ capital  */
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  /* NOTES: The context object is about the environment */
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "My Coffee Cart",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      /* NOTES: Column widgets can have several children, so it provides us
+        with a children argument. The items inside must be widgets. Also, in
+        terms of alignment. Containers will always align to the container with
+        the largest width. If you want to override this, use 
+        CrossAxisAlignment.stretch it will stretch the whole width available */
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text("How I like my Covfefe"),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const Text("Coffee prefs."),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/* NOTES: THE CONTAINER WIDGET - is a basic layout widget. 
+Containers act as wrappers to other widgets. When we use a container we can add
+margin and padding. By default, a containers width and height matches whatever a
+child's width and height is. But this can be overridden by specifying a 
+different width height. If it doesnt have a child it takes up all width and
+height   */
+
+/* Container(
+        color: Colors.orange,
+        //width: 200,
+        //height: 100,
+
+        /*NOTES: Edge Insets are how we set padding and margin. 
+        You can use .only to target a specific side */
+        padding: const EdgeInsets.all(20), //applies 20 padding all around
+        margin:
+            const EdgeInsets.fromLTRB(10, 40, 0, 0), //left, top, right, bot.
+        child: const Text(
+          "Hello, Covfefe Lovers",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 4,
+            decoration: TextDecoration.underline,
+            fontStyle: FontStyle.italic,
+          ),
+        )); */
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+//to import files you dont need to specify the path because its all in lib.
+//just the project name and the file unless its in a folder.
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+/* NOTES: lib is where most of your source code for the app lives. Screens/pages/
+services are common folders used by developers to oragnize their apps in lib.
+android/windows folder contain platform specific files. They're used under the
+hood by flutter for running or publishing our apps. We dont need to edit it or
+use it. Neither do we need to edit or use the build folder. .dart and .idea we
+can also ignore. The pubspec file is one of the only files you will edit. Its
+where you specify dependencies and paths to static assets like images    */
+
+/* Main and runApp are just the entrypoint to our app */
+void main() {
+/* NOTES: Text widgets require material ancestors  such as a scaffold 
+          to function correctly. You will see this in the docs */
+  runApp(const MaterialApp(
+/* NOTES: Text widgets require material ancestors  such as a scaffold 
+          to function correctly. You will see this in the docs */
+    home: Sandbox(),
+  ));
+}
+
+/* NOTES: The sandbox widget lets us experiment with a widget without 
+destroying the home screen. Just change MyWidget to Sandbox  */
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Sandbox"),
+          backgroundColor: Colors.grey,
+        ),
+        body: Row(
+          children: [
+            Container(
+                height: 100, color: Colors.green, child: const Text("one")),
+          ],
+        ));
+  }
+}
